257d0f4358f40ddf8872329eab0ee70088494299
Ricardo Sanchez <r.sanchez.macias.dev@gmail.com>
2024-02-26 15:16:33 -0800

    Add scaffold for project

---
+++ Cartera/Business/Services/JSONParser.swift
@@ -0,0 +1,10 @@
+//
+//  JSONParser.swift
+//  Cartera
+//
+//  Created by Ricardo Sanchez-Macias on 2/26/24.
+//
+
+import Foundation
+
+// TODO: - Add generic class for reading and parsing JSON files
